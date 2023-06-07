20c637823e30f2d4eea939a561490575b26879fc
Seth Musker <sethmusker@gmail.com>
2023-06-07 18:07:23 +0200

    Updated trimFasta (props and nums allowed)

--- herbchompernogaps_consensus_allseqs.R
+++ herbchompernogaps_consensus_allseqs.R
@@ -22,8 +22,8 @@ OPTIONAL:
  Global fasta trimming options:
 --trim_before [default=1] [Trim fasta before chomping? 1=yes, 0=no.]
 --trim_after [default=1] [Trim fasta after chomping? 1=yes, 0=no.]
--m / --min_n_sites_per_seq [default=100] [Before AND after chomping, *sequences* with <= 'min_n_sites_per_seq' non-gap sites will be removed. If < 1 this is interpreted as a proportion.]
--p / --min_prop_seqs_per_site [default=0.1] [After chomping, *sites* <= 'min_prop_seqs_per_site' non-gap sites (proportion) will be removed]
+-m / --min_sites_per_seq [default=100] [Before AND after chomping, *sequences* with <= 'min_sites_per_seq' non-gap sites will be removed. If < 1 this is interpreted as a proportion.]
+-p / --min_seqs_per_site [default=0.1] [After chomping, *sites* <= 'min_seqs_per_site' non-gap sites (proportion) will be removed. If < 1 this is interpreted as a proportion.]
  
  Consensus-building options:
 --consensus_prop_gaps_allowed [default=0.9] [Sites with > 'consensus_prop_gaps_allowed' will be coded as missing in the consensus]
@@ -74,19 +74,19 @@ if ("--append_consensus" %in% args && (trim_after != 0)) {
 }
 
 #before and after chomping, sequences with less than than this many non-gap sites will be removed
-if (any(grepl("-m",args) | grepl("--min_n_sites_per_seq",args))) {
-	args[which(args == "--min_n_sites_per_seq")] <- "-m"
-	MNPS <- as.numeric(args[which(args == "-m")+1])
+if (any(grepl("-m",args) | grepl("--min_sites_per_seq",args))) {
+	args[which(args == "--min_sites_per_seq")] <- "-m"
+	MinSitesPerSeq <- as.numeric(args[which(args == "-m")+1])
 } else {
-	MNPS <- 100
+	MinSitesPerSeq <- 100
 }
 
 # Sites with this many non-gap sites will be removed
-if (any(grepl("-p",args) | grepl("--min_prop_seqs_per_site",args))) {
-	args[which(args == "--min_prop_seqs_per_site")] <- "-p"
-	MPPS <- as.numeric(args[which(args == "-p")+1])
+if (any(grepl("-p",args) | grepl("--min_seqs_per_site",args))) {
+	args[which(args == "--min_seqs_per_site")] <- "-p"
+	MinSeqsPerSite <- as.numeric(args[which(args == "-p")+1])
 } else {
-	MPPS <- 0.1
+	MinSeqsPerSite <- 0.1
 }
 
 ## Options for consensus-making
@@ -134,7 +134,7 @@ getFastaWidth <- function(fa) unique(unlist(lapply(fa,length)))
 
 if (length(getFastaWidth(gene)) > 1) stop("Fasta has varying sequence lengths -- is it aligned?")
 
-trimFasta <- function(fa,min_n_sites_per_seq,min_prop_seqs_per_site) {
+trimFasta <- function(fa,min_sites_per_seq,min_seqs_per_site) {
 	# iteratively trims until there are
 	## 1. no empty sequences AND
 	## 2. no effectively empty sites
@@ -143,30 +143,35 @@ trimFasta <- function(fa,min_n_sites_per_seq,min_prop_seqs_per_site) {
 	iter <- 0
 	while (condition == TRUE) {
 	iter <- iter+1
-	cat("Trimming iteration\t",iter,"\n")
+	cat("\nTrimming iteration\t",iter,"\n")
 		# Find and remove gappy sites
-		is_empty_site <- !apply(do.call(rbind,fa),2,meetsPropNonGap,prop_nonGap=min_prop_seqs_per_site)
+		if (min_seqs_per_site > 1){
+			is_empty_site <- !apply(do.call(rbind,fa),2,meetsNumNonGap,num_nonGap=min_seqs_per_site)
+		} else {
+			is_empty_site <- !apply(do.call(rbind,fa),2,meetsPropNonGap,prop_nonGap=min_seqs_per_site)
+		}
+		# print(which(is_empty_site))
 		if (any(is_empty_site)) {
-			cat(sum(is_empty_site),"out of",fa_width,"SITES have <",min_prop_seqs_per_site*100,"% sequences with data (not N, not gap). Removing them.\n")
+			cat(sum(is_empty_site),"out of",fa_width,"SITES have <",min_seqs_per_site,"	sequences with data (not N, not gap). Removing them.\n")
 			fa <- lapply(fa,function(x) x[-which(is_empty_site)])
 			found_missing_sites<-TRUE
 		} else {
-			cat("No missing sites! Done trimming.\n")
+			cat("No (effectively) missing sites.\n")
 			found_missing_sites<-FALSE
 		}
 		# Find and remove gappy sequences
-		if (min_n_sites_per_seq>1) {
-			is_empty_seq <- !unlist(lapply(fa,meetsNumNonGap,num_nonGap=min_n_sites_per_seq))
+		if (min_sites_per_seq > 1) {
+			is_empty_seq <- !unlist(lapply(fa,meetsNumNonGap,num_nonGap=min_sites_per_seq))
 		} else {
-			is_empty_seq <- !unlist(lapply(fa,meetsPropNonGap,prop_nonGap=min_n_sites_per_seq))
+			is_empty_seq <- !unlist(lapply(fa,meetsPropNonGap,prop_nonGap=min_sites_per_seq))
 		}
 		if (any(is_empty_seq)) {
-			cat(sum(is_empty_seq),"out of",length(fa),"SEQUENCES have <",min_n_sites_per_seq,"sites with data (not N, not gap). Removing them.\n")
+			cat(sum(is_empty_seq),"out of",length(fa),"SEQUENCES have <",min_sites_per_seq,"sites with data (not N, not gap). Removing them.\n")
 			cat("Sequences removed:",names(fa)[is_empty_seq],sep = "\n")
 			found_missing_seqs<-TRUE
 			fa <- fa[!is_empty_seq]
 		} else {
-			cat("No empty sequences found in this iteration.\n")
+			cat("No (effectively) empty sequences.\n")
 			found_missing_seqs<-FALSE
 		}
 		if (!found_missing_sites && !found_missing_seqs) condition <- FALSE
@@ -178,77 +183,77 @@ trimFasta <- function(fa,min_n_sites_per_seq,min_prop_seqs_per_site) {
 fa_as_aln <- function(x) as.alignment(length(x),names(x),lapply(x,c2s),NA)
 
 consensus_ignoreGaps <- function(aln,prop_gaps_allowed,conflict_minor_prop_ignore) {
-  temp <- seqinr::consensus(aln,method="profile") # produces a matrix of character counts per site
-  rnames <- rownames(temp)
-  gaprow <- which(rnames == "-")
-  ngaprow <- which(rnames == "n")
-  if ("-" %in% rnames || "n" %in% rnames) {
-    temp_nonGap <- temp[-c(gaprow,ngaprow),]
-  } else {
-    temp_nonGap <- temp
-  }
-  rnames_nonGap <- rownames(temp_nonGap)
-  ## 1. Get the majority sequence, ignoring gaps
-  ##    note that for now ties are resolved arbitrarily, but later will be recoded as gaps
-  ##    due to the minor proportion ratio (which is bounded at [0,0.5])
-  majority_ignoreGaps <- unlist(apply(temp_nonGap,2,function(x) rnames_nonGap[which(x == max(x))][1]))
-  ## 2. Find SNPs (to recode as gaps eventually)
-  is_monomorphic <- apply(temp_nonGap,2,function(x) length(x[which(x!=0)]) == 1)
-  conflict_ratio <- apply(temp_nonGap,2,function(x) min(x[x!=0])[1] / sum(x[x!=0]))
-  passes_conflict_ratio <-  (conflict_ratio <= conflict_minor_prop_ignore)
-  ## 3. Get index of sites with < "prop_gaps_allowed" proportion gaps
-  prop_gaps <- apply(temp,2,function(x) (x[gaprow]+x[ngaprow]) / aln$nb)
-  passes_prop_gaps <- prop_gaps <= prop_gaps_allowed
-  cat("Generating majority-rule consensus sequence while requiring:\n\t(a). % of sequences with data:\t\t\t>",
-      100*(1-prop_gaps_allowed),"%\n\tAND\n\t(b). If ambiguous, % conflicting sequences:\t<",
-      conflict_minor_prop_ignore*100,"%\n")
-  ngap <- sum(majority_ignoreGaps == "-")
-  ## change sites not passing above filters to gaps
-  majority_ignoreGaps[!(passes_prop_gaps)]  <- "-"
-  ngap2 <- sum(majority_ignoreGaps == "-")
-  cat("Results:\n\t(a). Characters in the consensus recoded as gaps by missingness:\t",ngap2-ngap,"\n")
-  majority_ignoreGaps[!(passes_conflict_ratio) & !(is_monomorphic)] <- "-"
-  ngap3 <- sum(majority_ignoreGaps == "-")
-  cat("\tAND\n\t(b). Characters in the consensus recoded as gaps by ambiguity:\t",ngap3-ngap2,"\n")
-  cat("\nFinal consensus has ",length(majority_ignoreGaps)-ngap3,
-      " non-gaps out of a total of ",length(majority_ignoreGaps)," sites (",
-      signif(ngap3*100/length(majority_ignoreGaps),2),"% gaps).\n",sep="")
-  majority_ignoreGaps <- seqinr::as.SeqFastadna(majority_ignoreGaps)
-  attr(majority_ignoreGaps,"Annot") <- paste0(">HerbComper_consensus_gaps_",prop_gaps_allowed,"_minor_conflict_",conflict_minor_prop_ignore)
-  attr(majority_ignoreGaps,"name") <- paste0("HerbComper_consensus_gaps_",prop_gaps_allowed,"_minor_conflict_",conflict_minor_prop_ignore)
-  majority_ignoreGaps <- list(majority_ignoreGaps)
-  names(majority_ignoreGaps) <- paste0("HerbComper_consensus_gaps_",prop_gaps_allowed,"_minor_conflict_",conflict_minor_prop_ignore)
-  return(majority_ignoreGaps) ## <-- a named list. Use [[1]] index to get the sequence itself.
+	temp <- seqinr::consensus(aln,method="profile") # produces a matrix of character counts per site
+	rnames <- rownames(temp)
+	gaprow <- which(rnames == "-")
+	ngaprow <- which(rnames == "n")
+	if ("-" %in% rnames || "n" %in% rnames) {
+	temp_nonGap <- temp[-c(gaprow,ngaprow),]
+	} else {
+	temp_nonGap <- temp
+	}
+	rnames_nonGap <- rownames(temp_nonGap)
+	## 1. Get the majority sequence, ignoring gaps
+	##    note that for now ties are resolved arbitrarily, but later will be recoded as gaps
+	##    due to the minor proportion ratio (which is bounded at [0,0.5])
+	majority_ignoreGaps <- unlist(apply(temp_nonGap,2,function(x) rnames_nonGap[which(x == max(x))][1]))
+	## 2. Find SNPs (to recode as gaps eventually)
+	is_monomorphic <- apply(temp_nonGap,2,function(x) length(x[which(x!=0)]) == 1)
+	conflict_ratio <- apply(temp_nonGap,2,function(x) min(x[x!=0])[1] / sum(x[x!=0]))
+	passes_conflict_ratio <-  (conflict_ratio <= conflict_minor_prop_ignore)
+	## 3. Get index of sites with < "prop_gaps_allowed" proportion gaps
+	prop_gaps <- apply(temp,2,function(x) (x[gaprow]+x[ngaprow]) / aln$nb)
+	passes_prop_gaps <- prop_gaps <= prop_gaps_allowed
+	cat("Generating majority-rule consensus sequence while requiring:\n\t(a). % of sequences with data:\t\t\t>",
+		100*(1-prop_gaps_allowed),"%\n\tAND\n\t(b). If ambiguous, % conflicting sequences:\t<",
+		conflict_minor_prop_ignore*100,"%\n")
+	ngap <- sum(majority_ignoreGaps == "-")
+	## change sites not passing above filters to gaps
+	majority_ignoreGaps[!(passes_prop_gaps)]  <- "-"
+	ngap2 <- sum(majority_ignoreGaps == "-")
+	cat("Results:\n\t(a). Characters in the consensus recoded as gaps by missingness:\t",ngap2-ngap,"\n")
+	majority_ignoreGaps[!(passes_conflict_ratio) & !(is_monomorphic)] <- "-"
+	ngap3 <- sum(majority_ignoreGaps == "-")
+	cat("\tAND\n\t(b). Characters in the consensus recoded as gaps by ambiguity:\t",ngap3-ngap2,"\n")
+	cat("\nFinal consensus has ",length(majority_ignoreGaps)-ngap3,
+		" non-gaps out of a total of ",length(majority_ignoreGaps)," sites (",
+		signif(ngap3*100/length(majority_ignoreGaps),2),"% gaps).\n",sep="")
+	majority_ignoreGaps <- seqinr::as.SeqFastadna(majority_ignoreGaps)
+	attr(majority_ignoreGaps,"Annot") <- paste0(">HerbComper_consensus_gaps_",prop_gaps_allowed,"_minor_conflict_",conflict_minor_prop_ignore)
+	attr(majority_ignoreGaps,"name") <- paste0("HerbComper_consensus_gaps_",prop_gaps_allowed,"_minor_conflict_",conflict_minor_prop_ignore)
+	majority_ignoreGaps <- list(majority_ignoreGaps)
+	names(majority_ignoreGaps) <- paste0("HerbComper_consensus_gaps_",prop_gaps_allowed,"_minor_conflict_",conflict_minor_prop_ignore)
+	return(majority_ignoreGaps) ## <-- a named list. Use [[1]] index to get the sequence itself.
 }
 
 calcIdentity <- function(sequence, reference, start, window) {
-  s <-  sequence[start:(start+window)]
-  r <- reference[start:(start+window)]
-  s_is_r <- s == r
-  s_is_missing <- s %in% c("-","n")
-  r_is_missing <- r %in% c("-","n")
-  s_and_r_are_not_missing <- !s_is_missing & !r_is_missing
-  s_is_r_ignore_gap <- s_is_r & s_and_r_are_not_missing
-  sequence_identity <- sum(s_is_r_ignore_gap)/sum(s_and_r_are_not_missing)
-  if (is.na(sequence_identity)) sequence_identity <- 1 ## ignore empty windows. These will be trimmed by trimFasta()
-  return(sequence_identity)
+	s <-  sequence[start:(start+window)]
+	r <- reference[start:(start+window)]
+	s_is_r <- s == r
+	s_is_missing <- s %in% c("-","n")
+	r_is_missing <- r %in% c("-","n")
+	s_and_r_are_not_missing <- !s_is_missing & !r_is_missing
+	s_is_r_ignore_gap <- s_is_r & s_and_r_are_not_missing
+	sequence_identity <- sum(s_is_r_ignore_gap)/sum(s_and_r_are_not_missing)
+	if (is.na(sequence_identity)) sequence_identity <- 1 ## ignore empty windows. These will be trimmed by trimFasta()
+	return(sequence_identity)
 }
 
 ## Begin work! ----
 if (trim_before==1) {
-	cat("Performing initial fasta filtering to remove sequences with <",MNPS,"non-gap/non-N sites.\n")
-	cat("Note that no sites (except completely empty sites) will be removed in this round!\n")
-	gene <- trimFasta(gene,min_n_sites_per_seq=MNPS,min_prop_seqs_per_site=0)
+	cat("\nPerforming initial fasta filtering to remove sequences with <",MinSitesPerSeq,"non-gap/non-N sites.\n")
+	cat("Note that no sites (except sites with < 3 sequences) will be removed in this round!\n")
+	gene <- trimFasta(gene, min_sites_per_seq = MinSitesPerSeq, min_seqs_per_site = 3)
 } else {
 	## just trim to remove all-gap sequences and sites
-	cat("You have opted out of initial fasta filtering. Only empty sequences and sites will be trimmed.\n")
-	gene <- trimFasta(gene,min_n_sites_per_seq=0,min_prop_seqs_per_site=0)
+	cat("\nYou have opted out of initial fasta filtering. Only empty sequences and sites will be trimmed.\n")
+	gene <- trimFasta(gene, min_sites_per_seq = 0, min_seqs_per_site = 0)
 }
 
 geneAlCon <- consensus_ignoreGaps(
 	fa_as_aln(gene),
-	prop_gaps_allowed=consensus_prop_gaps_allowed,
-	conflict_minor_prop_ignore=consensus_minor_prop_ignore)
+	prop_gaps_allowed = consensus_prop_gaps_allowed,
+	conflict_minor_prop_ignore = consensus_minor_prop_ignore)
 
 if ("--write_consensus" %in% args) {
 	write.fasta(geneAlCon[[1]],
@@ -341,11 +346,12 @@ cat("\n**************************\n
 	Chomping finished! Next step is to remove any now-empty sequences and now-empty sites.\n")
 
 if (trim_after==1) {
-	gene <- trimFasta(gene,min_n_sites_per_seq=MNPS,min_prop_seqs_per_site=MPPS)
+	cat("\nPost-chomp trimming.\n\n")
+	gene <- trimFasta(gene, min_sites_per_seq = MinSitesPerSeq, min_seqs_per_site = MinSeqsPerSite)
 } else {
 	## just trim to remove all-gap sequences and sites
 	cat("You have opted out of fasta filtering after chomping. Only empty sequences and sites will be trimmed.\n")
-	gene <- trimFasta(gene,min_n_sites_per_seq=0,min_prop_seqs_per_site=0)
+	gene <- trimFasta(gene, min_sites_per_seq = 0, min_seqs_per_site = 0)
 }
 
 if ("--append_consensus" %in% args) {
@@ -353,4 +359,4 @@ if ("--append_consensus" %in% args) {
 }
 
 cat("Output fasta",outfile,"has",getFastaWidth(gene),"SITES.\n")
-write.fasta(lapply(gene,toupper),names=names(gene),file=outfile)
+write.fasta(lapply(gene,toupper), names=names(gene), file=outfile)
